1375b56763d9ca8259336f927380c17286b74bdb
Denis-D-S <denisduartedasilva@hotmail.com>
2022-02-10 09:55:57 -0300

    primeiro commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    //código inicial
+    }
+}
---
+++ src/com/company/Teste.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Teste {
+    //código inicial
+}
